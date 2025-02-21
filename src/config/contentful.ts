0ba9dde2a54cb93a3473785ae2b0e9e6c0e64f3e
Jean Paul Mayorga <jpmayorga@outlook.com>
2025-02-21 11:45:48 -0500

    fix: updating tokens

--- src/config/contentful.ts
+++ src/config/contentful.ts
@@ -1,15 +1,14 @@
 import { createClient as createContentful } from "contentful";
+import { env } from "./env";
 
-const env = process.env.VERCEL_ENV || "";
-const deliveryAccessToken =
-  process.env.X_CONTENTFUL_DELIVERY_ACCESS_TOKEN || "";
-const previewAccessToken = process.env.X_CONTENTFUL_PREVIEW_ACCESS_TOKEN || "";
-const spaceId = process.env.X_CONTENTFUL_SPACE_ID || "";
-
-export const isProduction = env === "production";
+const space = env.X_CONTENTFUL_SPACE_ID;
+const accessToken = env.isProduction
+  ? env.X_CONTENTFUL_LIVE_TOKEN
+  : env.X_CONTENTFUL_PREVIEW_TOKEN;
+const host = env.isProduction ? "cdn.contentful.com" : "preview.contentful.com";
 
 export const contentfulClient = createContentful({
-  space: spaceId,
-  accessToken: isProduction ? deliveryAccessToken : previewAccessToken,
-  host: isProduction ? "cdn.contentful.com" : "preview.contentful.com",
+  space,
+  accessToken,
+  host,
 }).withoutUnresolvableLinks;
