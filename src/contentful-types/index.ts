016dd918ff591ac1b62fd69067a597061573a7e9
Jean Paul Mayorga <jpmayorga@outlook.com>
2025-02-21 11:22:19 -0500

    fix:

--- src/contentful-types/index.ts
+++ src/contentful-types/index.ts
@@ -1,9 +1,41 @@
-export type { TypeComponent5050, TypeComponent5050Fields, TypeComponent5050Skeleton } from "./TypeComponent5050";
-export type { TypeComponentAccordion, TypeComponentAccordionFields, TypeComponentAccordionSkeleton } from "./TypeComponentAccordion";
-export type { TypeComponentAccordionItem, TypeComponentAccordionItemFields, TypeComponentAccordionItemSkeleton } from "./TypeComponentAccordionItem";
-export type { TypeComponentCarousel, TypeComponentCarouselFields, TypeComponentCarouselSkeleton } from "./TypeComponentCarousel";
-export type { TypeComponentRichImage, TypeComponentRichImageFields, TypeComponentRichImageSkeleton } from "./TypeComponentRichImage";
-export type { TypeComponentRichText, TypeComponentRichTextFields, TypeComponentRichTextSkeleton } from "./TypeComponentRichText";
-export type { TypePageBlogPost, TypePageBlogPostFields, TypePageBlogPostSkeleton } from "./TypePageBlogPost";
-export type { TypePages, TypePagesFields, TypePagesSkeleton } from "./TypePages";
+export type {
+  TypeComponent5050,
+  TypeComponent5050Fields,
+  TypeComponent5050Skeleton,
+} from "./TypeComponent5050";
+export type {
+  TypeComponentAccordion,
+  TypeComponentAccordionFields,
+  TypeComponentAccordionSkeleton,
+} from "./TypeComponentAccordion";
+export type {
+  TypeComponentAccordionItem,
+  TypeComponentAccordionItemFields,
+  TypeComponentAccordionItemSkeleton,
+} from "./TypeComponentAccordionItem";
+export type {
+  TypeComponentCarousel,
+  TypeComponentCarouselFields,
+  TypeComponentCarouselSkeleton,
+} from "./TypeComponentCarousel";
+export type {
+  TypeComponentRichImage,
+  TypeComponentRichImageFields,
+  TypeComponentRichImageSkeleton,
+} from "./TypeComponentRichImage";
+export type {
+  TypeComponentRichText,
+  TypeComponentRichTextFields,
+  TypeComponentRichTextSkeleton,
+} from "./TypeComponentRichText";
+export type {
+  TypePageBlogPost,
+  TypePageBlogPostFields,
+  TypePageBlogPostSkeleton,
+} from "./TypePageBlogPost";
+export type {
+  TypePages,
+  TypePagesFields,
+  TypePagesSkeleton,
+} from "./TypePages";
 export type { TypeSeo, TypeSeoFields, TypeSeoSkeleton } from "./TypeSeo";
