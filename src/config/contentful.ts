d670382862a95e65855fa579493a6ad25ab8e491
Jean Paul Mayorga <jpmayorga@outlook.com>
2025-02-27 20:49:57 -0500

    refactor: simplify Contentful environment configuration

--- src/config/contentful.ts
+++ src/config/contentful.ts
@@ -2,9 +2,7 @@ import { createClient as createContentful } from "contentful";
 import { env } from "./env";
 
 const space = env.X_CONTENTFUL_SPACE_ID;
-const accessToken = env.isProduction
-  ? env.X_CONTENTFUL_LIVE_TOKEN
-  : env.X_CONTENTFUL_PREVIEW_TOKEN;
+const accessToken = env.X_CONTENTFUL_TOKEN;
 const host = env.isProduction ? "cdn.contentful.com" : "preview.contentful.com";
 
 export const contentfulClient = createContentful({
